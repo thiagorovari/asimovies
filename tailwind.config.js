62f9e5376e392783e7cedadd260f4c0b9d04c404
plcrisp <pedrolcrisp@gmail.com>
2025-02-18 17:46:31 -0300

    🚑 Downgrade Angular e Tailwind adicionado

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'black': '#000000',
+      },
+    },
+  },
+  plugins: [],
+}
+
+
+
