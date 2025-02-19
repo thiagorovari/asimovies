9d2d8d123be2c6065ed0567106d2f5e4e57477af
Thiago <thiagorovari@gmail.com>
2025-02-19 00:58:41 -0300

    feat: integrate Firebase authentication and add environment configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDeZIs3tXstLccHF-uGjsGK2o-CJyYIvkM",
+    authDomain: "asimovies-6a4bb.firebaseapp.com",
+    projectId: "asimovies-6a4bb",
+    storageBucket: "asimovies-6a4bb.firebasestorage.app",
+    messagingSenderId: "693693136047",
+    appId: "1:693693136047:web:a8af87b9c435be6f1ae85a",
+    measurementId: "G-1RDPDFFSBW"
+  }
+}
